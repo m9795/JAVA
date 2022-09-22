c1fe7885dbce613da6d7e25301c6ee18ef12e756
m9795 <106650955+m9795@users.noreply.github.com>
2022-09-23 03:08:05 +0900

    [Add]Java_practice

---
+++ javaWcp/src/javaWcp/Chapter04.java
@@ -0,0 +1,12 @@
+package javaWcp;
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int NUMBER = 327;
+		String str = "ma";
+		System.out.println(str);
+		str = "おもち";
+		System.out.println(str);
+		System.out.println(NUMBER);
+	}
+}
---
+++ javaWcp/src/javaWcp/Chapter05.java
@@ -0,0 +1,22 @@
+package javaWcp;
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 10L;
+		System.out.println(l);
+		double d = 10.0d;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+		
+		Integer i = 100;
+		System.out.println(i);
+		Float f = 1.4142f;
+		System.out.println(f);
+		Character c = 'う';
+		System.out.println(c);
+		
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+	}
+}
